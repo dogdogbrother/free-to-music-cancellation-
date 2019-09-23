33e4f84e7d50e9ff5d150ac2e7105c8f84a4a7ff
dogdogbrother <gougewh@sina.com>
2019-09-23 22:09:36 +0800

    配置上线问题

---
+++ build/precache-manifest.b033c897a5a1a12ab89816f13bc0a348.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ef1a7b5e7a28103d1c2dda454c8de03",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ccccfefa68e287524007",
+    "url": "./static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "aa7e138198592d92e013",
+    "url": "./static/css/main.64b7e2ce.chunk.css"
+  },
+  {
+    "revision": "ccccfefa68e287524007",
+    "url": "./static/js/2.8306aa54.chunk.js"
+  },
+  {
+    "revision": "aa7e138198592d92e013",
+    "url": "./static/js/main.43f484d3.chunk.js"
+  },
+  {
+    "revision": "c59b33a809624d535eb4",
+    "url": "./static/js/runtime~main.ac23cd9c.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.99f943b78094ef71a731dc934be5dce8.js"
+  "./precache-manifest.b033c897a5a1a12ab89816f13bc0a348.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
