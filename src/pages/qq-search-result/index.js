dd9ac8102de99252a648a5f40d597323e06e34c4
dogdogbrother <gougewh@sina.com>
2019-09-28 20:33:58 +0800

    测试下线上的qq请求的接口

--- src/pages/qq-search-result/index.js
+++ src/pages/qq-search-result/index.js
@@ -8,7 +8,7 @@ const QQsearchResult = (props) => {
   const [songList, setSongList] = useState([]); 
   useEffect(()=>{
     let pathState = props.location.state
-    axios.get(`/qpi/c.y.qq.com/soso/fcgi-bin/client_search_cp?ct=24&qqmusic_ver=1298&new_json=1&remoteplace=txt.yqq.song&searchid=57753463161831399&t=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p=1&n=20&w=${pathState.searchVlue}&g_tk=5381&loginUin=0&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=0`)
+    axios.get(`https://bird.ioliu.cn/v2?url=https://c.y.qq.com/soso/fcgi-bin/client_search_cp?ct=24&qqmusic_ver=1298&new_json=1&remoteplace=txt.yqq.song&searchid=57753463161831399&t=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p=1&n=20&w=${pathState.searchVlue}&g_tk=5381&loginUin=0&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=0`)
     .then(res=>{
       setSongList(res.data.data.song.list.map(item=>{
         return {
