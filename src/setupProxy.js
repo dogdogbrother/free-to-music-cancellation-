38c5e30d1a1aebdf46ae4cd2f89c912c8d9b2208
tsdhr1 <gougewh@sina.com>
2019-11-06 15:32:33 +0800

    :see_no_evil:

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,21 +4,21 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
   app.use(proxy('/qpi', { 
-    target: 'http://148.70.108.11:8080/',
+    target: 'http://49.233.185.168:8080/',
     changeOrigin:true,
     pathRewrite: {
       "^/qpi": "/"
     }
   }))
   app.use(proxy('/wpi', { 
-    target: 'http://148.70.108.11:3000/',
+    target: 'http://49.233.185.168:3000/',
     changeOrigin:true,
     pathRewrite: {
       "^/wpi": "/"
     }
   }))
   app.use(proxy('/spi', {   // spi 的s就是森林的意思,我自己的接口
-    target: 'http://localhost:3030/',
+    target: 'http://49.233.185.168:3004/',
     changeOrigin:true,
     pathRewrite: {
       "^/spi": "/"
